96cd77f93c01f8867188823d56f69edd374954e1
Camilo Burbano <camiloburbano82@gmail.com>
2023-01-27 17:57:25 -0500

    primer commit

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 var app = new Vue({
-    el:'#app',
-    data:{
+    el: '#app',
+    data: {
+        cargos: [
+            { cargo: 'Secretario', salarioBase: 0 },
+            { cargo: 'Vendedor', salarioBase: 0 },
+            { cargo: 'Ensamblador', salarioBase: 0 }
+        ],
+        cargoSeleccionado: '',
+        salarioBase: '',
+        cantidadMaximaZapatos:''
 
-    }, 
-    methods:{
-        
+    },
+
+    methods: {
+
+        guardarSalarioBase() {
+
+            this.cargos.forEach(item => {
+
+                if (item.cargo == this.cargoSeleccionado) {
+                    item.salarioBase = this.salarioBase;
+                }
+
+            });
+
+            console.log(this.cargos);
+
+        },
+
+        guardarMaximoZapatos(){
+            console.log(this.cantidadMaximaZapatos);
+        }
+
     }
 }
 )
